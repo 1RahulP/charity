a1058c882cca55f986d15320b33c2002e314ac42
1RahulP <007rahulpundir@gmail.com>
2024-12-28 17:35:55 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,14 +21,14 @@ export default function Home() {
           <h2 className="text-xl font-semibold text-primary-500 mb-6">ABOUT US</h2>
           <h3 className="font-semibold text-4xl max-w-[650px] mb-10">Together, we can save more lives with your support.</h3>
           <p className="my-2 text-gray-400">Welcome to the Muslim Community of India, a symbol of compassion guided by the values of Islamic charity. Here, we embrace the transformative impact of faith-inspired giving to create meaningful change in the lives of those in need.</p>
-          <p className="text-gray-400">Our mission is clear and impactful – to uplift lives through kindness and generosity. Driven by the principles of compassion, fairness, and care, we aim to create a meaningful difference for individuals and communities, regardless of their race, faith, or background.</p>
+          <p className="text-gray-400">Our mission is clear and impactful to uplift lives through kindness and generosity. Driven by the principles of compassion, fairness, and care, we aim to create a meaningful difference for individuals and communities, regardless of their race, faith, or background.</p>
         </div>
       </div>
       <div className="px-4 py-12 max-w-[1150px] mx-auto px-4 py-12">
         <h2 className="text-2xl font-semibold text-center text-primary-500 mb-20">Our Top Initiatives.</h2>
         <div className="grid grid-cols-[1fr_2fr]">
           <div className="max-w-[300px]">
-            <h2 className="text-4xl font-semibold mb-2">WE'RE ON A <br /> <span className="text-primary-500"> MISSION</span> TO</h2>
+            <h2 className="text-4xl font-semibold mb-2">WE&apos;RE ON A <br /> <span className="text-primary-500"> MISSION</span> TO</h2>
             <p className="text-gray-400">solve the problems and reinvent charity for a new generation.</p>
           </div>
           <div className="grid grid-cols-2 gap-8">
